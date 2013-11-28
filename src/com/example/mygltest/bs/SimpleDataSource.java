f9719207fef92bcd6a57779aa25be54748e60224
hubin <up2dream@gmail.com>
2013-11-28 21:43:19 +0800

    ds file.

---
+++ src/com/example/mygltest/bs/SimpleDataSource.java
@@ -0,0 +1,21 @@
+package com.example.mygltest.bs;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class SimpleDataSource implements DataSource {
+	
+	public int getWidth() {
+		return 30000;
+	}
+	
+	public int getHeight() {
+		return 20000;
+	}
+
+	public void draw(Canvas canvas, int updateX, int updateY, Paint paint) {
+		canvas.drawColor(Color.rgb(255, 255, 0));
+	}
+
+}
